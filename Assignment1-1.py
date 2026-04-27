6ae5d17e949dcbe66940149231a9ba0d135da04a
Srinadh-Kotapati <srinadh.k@atenas.in>
2026-04-27 16:04:39 +0530

    Create Assignment1-1.py
    Performing different methods on lists

---
+++ Assignment1-1.py
@@ -0,0 +1,6 @@
+students = ["first","second","Third","Fourth","Fifth"]
+print(students)                      #original list
+students.append("sixth")             # Adding value to list
+students.remove(students[1])         #Removing value from list
+students.pop(2)                      #Removing value by using pop method
+print(students)
