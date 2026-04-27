6a8ba1e3c3f7fd2abfebbc4dc6dcf7ad4152ab92
Srinadh-Kotapati <srinadh.k@atenas.in>
2026-04-27 16:24:51 +0530

    Create Assignment1-9.py
    Explain the difference between list, tuple, set, and dictionary with one real-time example each.

---
+++ Assignment1-9.py
@@ -0,0 +1,26 @@
+#List
+a = ["a","b","c","d","e"]
+
+#used to store values . A list stores multiple items in order and can be changed.
+#Ex : Shopping cart items in stores
+
+#tuple
+location = (17.3850, 78.4867,13.54674,16.64748)
+
+#A tuple is like a list but cannot be changed after creation.
+#used for GPS coordinates (latitude, longitude).
+
+#set
+c = {1,3,4,5,6,7}
+#Used to store unique values
+
+#Dict
+student = {
+    "id": 1,
+    "name": "srinadh",
+    "marks": 100,
+    "subject":"Python"
+}
+
+#Used to store values in key-value pair
+#Database
