0ceb1981410b93ba013bf593a6231dad8e124a12
Srinadh-Kotapati <srinadh.k@atenas.in>
2026-04-27 16:21:22 +0530

    Create Assignment1-7.py
    Declare two sets, perform and display: Union, Intersection, Difference.

---
+++ Assignment1-7.py
@@ -0,0 +1,19 @@
+# Defining two sets
+a = {1, 2, 3, 4, 5, 6, 7}
+b = {6, 7, 8, 9, 10}
+
+# Printing sets
+print("Set A:", a)
+print("Set B:", b)
+
+# printing a Union b
+print( a | b)
+
+#Printing a Intersection b
+print( a & b)
+
+# Printing a Difference b
+print( a - b)
+
+# Printing b Difference a
+print( b - a)
