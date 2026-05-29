8978c3c95888c9dc0d96ef563f4eb7fac71946f7
Srinadh-Kotapati <srinadh.k@atenas.in>
2026-05-29 19:05:10 +0530

    lists

--- lists.py
+++ lists.py
@@ -1,4 +1,4 @@
-
+'''
 n = int(input("Enter number of elements:"))
 l =[]
 for i in range(n):
@@ -8,3 +8,68 @@ print(l)
 
 
 a = list(map(int,input().split()[:3]))
+
+
+#reversing a list
+a = [1,2,3,4,5,6]
+mid = len(a)//2
+print(mid)
+inc = a[:mid]
+dec = a[mid:]
+inc.reverse()
+dec.reverse()
+r = inc+dec
+print(r)
+
+#sum = target
+l = [0,2,3,4,5,6,7,8]
+target = 8
+for i in range(0,len(l)):
+    for j in range(0,len(l)-1):
+        if l[i]+l[j] == target:
+            print((i,j))
+
+            
+
+#sorting values
+l = [1,2,0,1,0,2,1,0]
+for i in range(0,len(l)):
+    for j in range(0,len(l)-1):
+        if l[j] > l[j+1]:
+            l[j],l[j+1] = l[j+1],l[j]
+print(l)
+
+
+#reversing an array
+a = [1,2,3,4,5,6]
+a.reverse()
+print(a)
+
+#printing union values using loops
+a = [2,4,5,6,7]
+b = [4,5]
+c = []
+d = []
+for i in a:
+    for j in b:
+        if i == j:
+            c.append(i)
+for i in a:
+    for j in b:
+        if i != j:
+            d.append(i)
+            
+
+print(f"union {c}")
+print(f"intersection {set(d)}")
+'''
+
+# [1, 2, 3, 4, 5].....Output: [5, 1, 2, 3, 4]
+l = [1,2,3,4,5,6]
+for i in range(0,len(l)-1):
+    i = i+1
+print(l)
+
+
+
+
